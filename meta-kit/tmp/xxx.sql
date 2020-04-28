7496102ca52d812ad4efa59dcc42ec92a89ea440
liyaguang258 <542127896@qq.com>
2020-04-28 14:51:31 +0800

    xxx

---
+++ meta-kit/tmp/xxx.sql
@@ -0,0 +1,4 @@
+INSERT INTO  questionrecord (`mobile`,`invitecode`,`batch`,`status`) VALUES ('null','null','null','null');
+
+UPDATE `platf_oth`.`questionrecord` SET invitecode = 76ITMJ , batch = 第一次封测 , status = 10WHERE mobile = 18810259286 ;
+SET invitecode = HB9YOU , batch = 第二次封测 , status = 10WHERE mobile = 18166560481 ;
---
+++ meta-kit/tmp/邀请码_04-28_吴文俊.sql
@@ -0,0 +1,3 @@
+INSERT INTO `v09x_platf_core`.`userinvitecode` (`invitecode`,`userid`,`createtime`) VALUES 
+('E1MR2N',10000,1588036838500),
+('D2FOOG',10000,1588036838502),;
