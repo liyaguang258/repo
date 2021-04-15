19042f2f7a7c06654e01ff467327f70ad7727db2
liyaguang258 <542127896@qq.com>
2021-04-15 15:13:34 +0800

    校验ios下载地址是否正确

--- meta-kit/tmp/邀请码_04-28_吴文俊.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `v09x_platf_core`.`userinvitecode` (`invitecode`,`userid`,`createtime`) VALUES 
-('E1MR2N',10000,1588036838500),
-('D2FOOG',10000,1588036838502),;
--- meta-kit/tmp/邀请码_04-29_严谨.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `v09x_platf_core`.`userinvitecode` (`invitecode`,`userid`,`createtime`) VALUES 
-('UGZI4R',10000,1588123076739),
-('62XL8V',10000,1588123076741),
-('OFZK31',10000,1588123076744);
